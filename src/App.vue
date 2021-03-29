71023e855377aef4e1cb7a15d3eca8e79c97ca3e
Zavizhenetc <zavizhenetc@gmail.com>
2021-03-29 20:12:55 +0300

    add pagination, history.pushState

--- src/App.vue
+++ src/App.vue
@@ -45,9 +45,33 @@
       <!--      tickers -->
       <template v-if="tickers.length > 0">
         <hr class="w-full border-t border-gray-600 my-4"/>
+        <div>
+          <button
+              class="mx-2 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4
+              font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2
+              focus:ring-offset-2 focus:ring-gray-500"
+              @click="page = page -1 "
+              v-if="page > 1"
+          >
+            &#8592;
+          </button>
+          <button
+              class="mx-2 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4
+              font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2
+               focus:ring-offset-2 focus:ring-gray-500"
+              @click="page = page +1 "
+              v-if="hasNextPage"
+          >
+            &#8594;
+          </button>
+          <div>
+            Фильтр: <input v-model="filter"/>
+          </div>
+        </div>
+        <hr class="w-full border-t border-gray-600 my-4"/>
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-              v-for="t in tickers"
+              v-for="t in filteredTickers()"
               v-bind:key="t.name"
               @click="select(t)"
               :class="{ 'border-4': sel === t }"
@@ -137,10 +161,21 @@ export default {
       ticker: "",
       tickers: [],
       sel: null,
-      graph: []
+      graph: [],
+      page: 1,
+      filter: "",
+      hasNextPage: true,
+
     };
   },
   created() {
+   const windowData = Object.fromEntries(new URL(window.location).searchParams.entries());
+   if(windowData.filter){
+     this.filter = windowData.filter;
+   }
+   if(windowData.page){
+     this.page = windowData.page;
+   }
     const tickersData = localStorage.getItem('cryptonomicon-list');
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
@@ -150,6 +185,15 @@ export default {
     });
   },
   methods: {
+    filteredTickers() {
+      const startPage = (this.page - 1) * 6;
+      const endPage = this.page * 6;
+
+      const filteredTickers = this.tickers.filter(ticker => ticker.name.includes(this.filter));
+      this.hasNextPage = filteredTickers.length > endPage;
+      return filteredTickers.slice(startPage, endPage);
+    },
+
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -172,6 +216,7 @@ export default {
       this.tickers.push(currentTicker);
       localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
       this.subscribeToUpdates(currentTicker.name);
+      this.filter = "";
 
     },
 
@@ -189,6 +234,16 @@ export default {
       this.sel = ticker;
       this.graph = [];
     }
+  },
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`);
+    },
+    page(){
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`);
+    }
+
   }
 };
 </script>
